261bf2c708a721a6d7fc33d394e0ded8b87317f8
pjwolfh <admin@liceocristianorealintegral.edu.gt>
2025-06-30 19:57:18 -0600

    correción comillas en asistencias.service.ts

--- src/app/services/asistencias.service.ts
+++ src/app/services/asistencias.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AsistenciasService {
 
-  private apiUrl = `${environment.apiUrl}/api/asistencias'; // Cambia si tu backend usa otro puerto
+  private apiUrl = `${environment.apiUrl}/api/asistencias`; // Cambia si tu backend usa otro puerto
 
   constructor(private http: HttpClient) {}
 
